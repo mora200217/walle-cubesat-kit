c29e46011392242d5c31cff40e8558794e2d6c16
dimaldonadom <dimaldonadom@unal.edu.co>
2025-02-19 22:17:30 -0500

    Update BME280.py

--- walle_sensors/BME280.py
+++ walle_sensors/BME280.py
@@ -2,7 +2,7 @@ import board
 from adafruit_bme280 import basic as adafruit_bme280
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c,0x76)
 
 print("\nTemperature: %0.1f C" % bme280.temperature)
 print("Humidity: %0.1f %%" % bme280.humidity)
