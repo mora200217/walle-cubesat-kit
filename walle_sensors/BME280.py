15e84ffc2a050749ff31f8964c3d5cd570655fee
dimaldonadom <dimaldonadom@unal.edu.co>
2025-02-21 15:50:39 -0500

    Update BME280.py

--- walle_sensors/BME280.py
+++ walle_sensors/BME280.py
@@ -8,12 +8,18 @@ from adafruit_bme280 import basic as adafruit_bme280
 class BME280(Sensor):
     def __init__(self, addr):
         super().__init__()
+
+        self.units = ["°C", '%', "hPa"]
+
         i2c = board.I2C()
-        self.bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c,addr)
+
+        try:
+            self.bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c,addr)
+        except Exception as e:
+            print(f"Error: Sensor BME280 no conectado - {e}")  # Mensaje de error opcional
 
 
     def setup(self) -> bool:
-        self.units = ["°C",'%',"hPa"]
         print("--Sensor BME280 ok--")
         return True
 
@@ -21,5 +27,10 @@ class BME280(Sensor):
         return np.array([self.bme280.temperature, self.bme280.humidity, self.bme280.pressure])
 
     def available(self):
-        return True
+        try:
+            _ = self.bme280.temperature  # Intenta leer un valor del sensor
+            return True
+        except Exception as e:
+            print(f"Error: Sensor desconectado - {e}")  # Mensaje de error opcional
+            return False
 
